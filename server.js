9d953e0f055b161472ea2326de624a867067ac9a
VEERESH069 <116574954+VEERESH069@users.noreply.github.com>
2025-01-24 10:51:01 +0530

    1stpush

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Status Code API is running on http://localhost:${PORT}`);
+});
+app.get('/status', (req, res) => {
+    res.send({ status: 'API is running' });
+});
